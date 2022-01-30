20a1d6c0d122d64c83fe987cb357efef90018a05
Vadim Evseev <chiffa117@gmail.com>
2022-01-30 21:57:33 +0300

    remove terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
-import { terser } from 'rollup-plugin-terser'
 
 const name = require('./package.json').main.replace(/\.js$/, '')
 
@@ -14,7 +13,6 @@ export default [
   bundle({
     plugins: [
       esbuild(),
-      terser(),
     ],
     output: [
       {
